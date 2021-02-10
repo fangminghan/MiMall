2005cd339d8e8d6def7965312063b01592d0e534
fangminghan <963601657@qq.com>
2021-02-10 20:57:09 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data(){
+    return {
+     
+    }
+  },
 }
 </script>
 
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports={
+    devServer:{
+        host:'localhost',
+        post:8080,
+        proxy:{
+            
+        }
+    }
+}
